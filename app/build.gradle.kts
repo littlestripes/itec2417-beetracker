6f9d64eacc43a1fcbb438d4f5ed029cdb67a7156
Elijah Kamali <94736197+littlestripes@users.noreply.github.com>
2024-12-04 12:44:17 -0600

    feat: implement image capture functionality
    - Added Firebase Storage dependencies.
    - Implemented camera functionality in `BeeSightingUserInput.kt` to capture and display images.
    - Modified `AndroidManifest.xml` to include permissions for storage access and a `FileProvider`.
    - Configured file paths for storing captured images.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,8 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
+
+    id("com.google.gms.google-services")
 }
 
 android {
@@ -36,12 +38,15 @@ android {
 }
 
 dependencies {
+    implementation(platform(libs.firebase.bom))
 
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
+    implementation(libs.firebase.storage.ktx)
+    implementation(libs.squareup.picasso)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
