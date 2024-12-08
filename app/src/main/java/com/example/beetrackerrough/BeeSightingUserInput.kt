6d1602cd73fdca2c9eb47bc121c88d41b4df04a8
Admin <156264657+Jdono18@users.noreply.github.com>
2024-12-08 09:35:59 -0600

    Modified result canceled library import Added upload image to firebase storage function

--- app/src/main/java/com/example/beetrackerrough/BeeSightingUserInput.kt
+++ app/src/main/java/com/example/beetrackerrough/BeeSightingUserInput.kt
@@ -1,13 +1,14 @@
 package com.example.beetrackerrough
 
 import android.annotation.SuppressLint
+import android.app.Activity.RESULT_CANCELED
 import android.app.Activity.RESULT_OK
 import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
 import android.os.Environment
 import android.provider.MediaStore
-import android.telephony.MbmsDownloadSession.RESULT_CANCELLED
+//import android.telephony.MbmsDownloadSession.RESULT_CANCELLED
 import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -104,6 +105,7 @@ class BeeSightingUserInput : Fragment() {
 
     // holding code to be replaced with send data to view model - linked to repository - upload to database
         submitButton.setOnClickListener {
+            uploadImage()
             parentFragmentManager
                 .beginTransaction()
                 .replace(R.id.bee_fragment_container, BeeBootScreen.newInstance(), "BEEBOOTSCREEN").addToBackStack("Beebootscreen")
@@ -167,7 +169,7 @@ class BeeSightingUserInput : Fragment() {
                 Log.d(TAG, "Image at $newPhotoPath")
                 visibleImagePath = newPhotoPath
             }
-            RESULT_CANCELLED -> {
+            RESULT_CANCELED -> {
                 Log.d(TAG, "No picture taken")
             }
         }
@@ -187,6 +189,17 @@ class BeeSightingUserInput : Fragment() {
                     Log.e(TAG, "Error loading $photoFilePath", e)
                 }
             })
+    }
+
+    private fun uploadImage() {
+        if (photoUri != null && imageFilename != null) {
+
+            val imageStorageRootRef = storage.reference
+            val imagesRef = imageStorageRootRef.child("images")
+            val imageFileRef = imagesRef.child(imageFilename!!)
+
+            imageFileRef.putFile(photoUri!!)
+        }
     }
 
     companion object {
